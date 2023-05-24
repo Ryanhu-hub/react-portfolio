b149f091b3fcc0735b60cb3a8d4d0d79ff9b6f4e
Ryanhu-hub <ryanhuot20@gmail.com>
2023-05-24 15:36:27 -0700

    added basic ul and li for navbar

--- src/App.js
+++ src/App.js
@@ -4,20 +4,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <head className="App-header">
+      <title>
+        The end
+      </title>
+      </head>
+      <main>
+        <section>
+          <nav className='py-4 bg-cyan-100 flex justify-between'>
+          <h2>Ryanhu-Hub</h2>
+            <ul className="flex-time-center pr-5">
+              <li>
+                <a href="#">Github</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+            </ul>
+          </nav>
+        </section>
+      </main>
     </div>
   );
 }
